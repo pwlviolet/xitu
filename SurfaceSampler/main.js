0377eeab9d316e7f1291ad71552da059506010a3
wl_p <1169664352@qq.com>
2024-01-27 23:33:28 +0800

    feat:init surfacesampler

---
+++ SurfaceSampler/main.js
@@ -0,0 +1,55 @@
+import './style.css';
+import * as THREE from 'three';
+import { gsap } from 'gsap';
+import CameraControls from 'camera-controls';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
+
+import { RGBShiftShader } from 'three/addons/shaders/RGBShiftShader.js';
+import { DotScreenShader } from 'three/addons/shaders/DotScreenShader.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
+import { ClearPass } from 'three/addons/postprocessing/ClearPass.js';
+const canvas = document.getElementById('three-canvas');
+CameraControls.install({ THREE: THREE });
+let cameracontrols;
+let clock = new THREE.Clock();
+const width = canvas.clientWidth;
+const height = canvas.clientHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+camera.position.set(0, 0, 10);
+renderer.setSize(width, height);
+canvas.appendChild(renderer.domElement);
+renderer.render(scene, camera);
+
+cameracontrols = new CameraControls(camera, renderer.domElement);
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+  })
+);
+scene.add(box);
+
+let animate = function () {
+  requestAnimationFrame(animate);
+  cameracontrols.update(clock.getDelta());
+
+  // renderer.render(scene, camera);
+  composer.render();
+};
+
+animate();
+
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  composer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
